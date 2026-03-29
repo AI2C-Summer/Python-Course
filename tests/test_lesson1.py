5d3da87e91d166a2989fd76e089c8b0253f2ca53
FiveThirtyEight <136464184+CyberDave80@users.noreply.github.com>
2026-03-29 03:14:14 -0400

    Implement test for lesson1.py
    Add a test for lesson1.py to check output.

---
+++ tests/test_lesson1.py
@@ -0,0 +1,11 @@
+import subprocess
+import sys
+
+def test_lesson1():
+    result = subprocess.run(
+        [sys.executable, "lesson1.py"],  # uses correct Python version
+        capture_output=True,
+        text=True
+    )
+    #tests any non-empty string
+    assert result.stdout.strip() == ""
